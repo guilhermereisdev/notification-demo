26954a06beea3a88ffcc5effe0e5ac616ea8959f
Guilherme Reis <guilhermereis2009@hotmail.com>
2023-05-03 01:02:21 -0300

    Implementa o comportamento abrir uma activity que recebe e exibe os dados inseridos na notificação.

--- app/src/main/java/com/guilhermereisdev/notificationdemo/MainActivity.kt
+++ app/src/main/java/com/guilhermereisdev/notificationdemo/MainActivity.kt
@@ -9,12 +9,16 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
 
 class MainActivity : AppCompatActivity() {
 
     private val channelId = "com.guilhermereisdev.notificationdemo.channel1"
     private var notificationManager: NotificationManager? = null
+    private val KEY_REPLY = "key_reply"
+    private val notificationId = 45
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,18 +32,17 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             displayNotification()
         }
+        receiveInput()
     }
 
     private fun displayNotification() {
-        val notificationId = 45
-
         // Ao clicar na notificação leva para a MainActivity
         val tapResultIntent = Intent(this, MainActivity::class.java)
         val pendingIntent: PendingIntent = PendingIntent.getActivity(
             this,
             0,
             tapResultIntent,
-            PendingIntent.FLAG_IMMUTABLE
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
         )
 
         // Botão "Details" na notificação que leva para a DetailsActivity
@@ -64,6 +67,16 @@ class MainActivity : AppCompatActivity() {
         val actionSettings: NotificationCompat.Action =
             NotificationCompat.Action.Builder(0, "Settings", pendingIntentSettings).build()
 
+        // Exibe um textInput na notificação para o usuário inserir alguma informação
+        val remoteInput: RemoteInput = RemoteInput.Builder(KEY_REPLY).run {
+            setLabel("Insira seu nome")
+            build()
+        }
+        val replyAction: NotificationCompat.Action = NotificationCompat.Action.Builder(
+            0, "Responder", pendingIntent
+        ).addRemoteInput(remoteInput)
+            .build()
+
         // Cria a notificação
         val notification = NotificationCompat.Builder(this@MainActivity, channelId)
             .setContentTitle("Demo title")
@@ -71,9 +84,10 @@ class MainActivity : AppCompatActivity() {
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
+//            .setContentIntent(pendingIntent)
             .addAction(actionDetails)
             .addAction(actionSettings)
+            .addAction(replyAction)
             .build()
         notificationManager?.notify(notificationId, notification)
     }
@@ -89,6 +103,24 @@ class MainActivity : AppCompatActivity() {
                 description = channelDescription
             }
             notificationManager?.createNotificationChannel(channel)
+        }
+    }
+
+    private fun receiveInput() {
+        val intent = this.intent
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+        if (remoteInput != null) {
+            val inputString = remoteInput.getCharSequence(KEY_REPLY).toString()
+            val tvInputFromNotification: TextView = findViewById(R.id.tvInputStringFromNotification)
+            tvInputFromNotification.apply {
+                text = inputString
+            }
+            // Cria a resposta para aparecer na própria notificação após ser inserida uma informação
+            val repliedNotification = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentText("Resposta enviada com sucesso")
+                .build()
+            notificationManager?.notify(notificationId, repliedNotification)
         }
     }
 }
